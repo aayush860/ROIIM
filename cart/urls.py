2c5b79ca644ee837b738945d936f15eab5ea913a
Aayush Bhargava <aayush.bhrgv@gmail.com>
2020-07-17 21:16:16 +0530

    commit 2

--- cart/urls.py
+++ cart/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('checkout/', views.checkout),
     path('window/', views.windows),
     path('logout/', views.logout),
+    path('payment_sucessful/', views.payment_sucessful),
+    path('payment_unsucessful/', views.payment_unsucessful),
+    path('payment_handle_not_created/', views.payment_handle_not_created),
+
+    path('', views.windows),
+
 ]
--- cart/views.py
+++ cart/views.py
@@ -6,16 +6,42 @@ from django.db import IntegrityError
 
 
 def checkout(request):
-    if request.user.is_authenticated:
-        total_amount = (request.GET.get('cart_total',96))
-        user_det = user_details.objects.get(username=request.user)
-        billing_det = billing_details.objects.get(username=request.user)
-        config = configuration_key.objects.get(id=1)
-        return render(request, 'checkout.html', {'amount':total_amount,'user_det':user_det, 'billing_det':billing_det, 'config':config})
+        if request.user.is_authenticated:
+                total_amount = (request.GET.get('cart_total',96))
+                user_det = user_details.objects.get(username=request.user)
+                billing_det = billing_details.objects.get(username=request.user)
+                config = configuration_key.objects.get(id=1)
+                return render(request, 'checkout.html', {'amount':total_amount,'user_det':user_det, 'billing_det':billing_det, 'config':config})
+        else:
+                return redirect('/signin/')
+
 
 def windows(request):
-    return render(request, 'window.html')
+        if request.user.is_authenticated:
+                return render(request, 'window.html')
+        else:
+                return redirect('/signin/')
 
 def logout(request):
     auth.logout(request)
     return redirect('/signin/')
+
+
+
+def payment_sucessful(request):
+        if request.user.is_authenticated:
+                return render(request, 'payment_sucessful.html')
+        else:
+                return redirect('/signin/')
+
+def payment_unsucessful(request):
+        if request.user.is_authenticated:
+                return render(request, 'payment_unsucessful.html')
+        else:
+                return redirect('/signin/')
+
+def payment_handle_not_created(request):
+        if request.user.is_authenticated:
+                return render(request, 'payment_handle_not_ceated.html')
+        else:
+                return redirect('/signin/')
