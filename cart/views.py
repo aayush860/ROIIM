e396fcc0e3e62880b9fc4c1c6ba43d49a990d10a
Aayush Bhargava <aayush.bhrgv@gmail.com>
2020-07-18 17:40:51 +0530

    xyz

--- cart/views.py
+++ cart/views.py
@@ -7,16 +7,20 @@ from django.db import IntegrityError
 
 def checkout(request):
         if request.user.is_authenticated:
-                if request.method=='POST':
+                print (request.POST['payload'])
+                if request.POST.get('payload', -1)==-1:
+                        return redirect('/window/')
+                elif request.POST.get('firstName', -1)!=-1:
                         user_det = user_details.objects.filter(username=request.user).update(first_name=request.POST['firstName'], last_name=request.POST['lastName'], email=request.POST['email'], phone=request.POST['phone'], dob=request.POST['DOB'])
                         billing_det = billing_details.objects.filter(username=request.user).update(street=request.POST['address1'], street2=request.POST['address2'], city=request.POST['city'], zip_code=request.POST['zipcode'])
-                        return redirect('/checkout/')
-                else:
-                        total_amount = (request.GET.get('cart_total',96))
-                        user_det = user_details.objects.get(username=request.user)
-                        billing_det = billing_details.objects.get(username=request.user)
-                        config = configuration_key.objects.get(id=1)
-                        return render(request, 'checkout.html', {'amount':total_amount,'user_det':user_det, 'billing_det':billing_det, 'config':config})
+                    
+                
+                
+                print (request.POST['payload'])
+                user_det = user_details.objects.get(username=request.user)
+                billing_det = billing_details.objects.get(username=request.user)
+                config = configuration_key.objects.get(id=1)
+                return render(request, 'checkout.html', {'amount':request.POST['payload'],'user_det':user_det, 'billing_det':billing_det, 'config':config})
         else:
                 return redirect('/signin/')
 
